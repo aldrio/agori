07f3df0ca843274ec7e4f727f288bcd8a4e62d08
Brandon Aldrich <brandon@aldr.io>
2020-09-08 09:16:50 -0400

    Render every possible piece
    * Outputs straight into app source
    * Outputs ts file cataloging files

--- avatars/src/bin/generate-pieces.ts
+++ avatars/src/bin/generate-pieces.ts
@@ -9,97 +9,100 @@ import {
   PieceVariantId,
 } from '../data'
 
+function cartesianProduct<T>(...allEntries: T[][]): T[][] {
+  return allEntries.reduce<T[][]>(
+    (results, entries) =>
+      results
+        .map((result) => entries.map((entry) => result.concat([entry])))
+        .reduce((subResults, result) => subResults.concat(result), []),
+    [[]]
+  )
+}
+
 /**
  * Generate Pieces generates a directory of images with every Avatar Piece variant -> modifier combination
  */
-;(async () => {
-  const OUT_DIR = './output'
-  const OUT_SIZE = 100
+; (async () => {
+  const OUT_DIR = '../app/src/components/AvatarDesignCustomizer/pieces'
+  const OUT_SIZE = 300
+  const OUT_MODIFIER_SIZE = 90
   console.log(`Generating pieces to ${OUT_DIR}`)
 
-  await fs.mkdir(OUT_DIR, { recursive: true })
+  // await fs.mkdir(OUT_DIR, { recursive: true })
+
+  const keys: string[] = []
 
   /**
    * Runs addPieceVariant for every variant on this piece
    */
-  async function addPiece(
-    existingDesign: AvatarDesignData,
-    existingKey: string,
-    pieceId: PieceId,
-    rootPieceId?: PieceId,
-    rootPieceVariantId?: PieceVariantId
-  ) {
-    const piece = Pieces[pieceId]
-    const variants = Object.keys(piece.variants)
-    await Promise.all(
-      variants.map(async (vid) => {
-        await addPieceVariant(
-          existingDesign,
-          existingKey,
-          pieceId,
-          vid,
-          rootPieceId || pieceId,
-          rootPieceVariantId || vid
-        )
-      })
-    )
-  }
-
-  /**
-   * Writes the image if this piece variant is a leaf node in the avatar design graph
-   * Otherwise it recurses and runs addPiece for every possible modification of the current piece variant
-   */
   async function addPieceVariant(
-    existingDesign: AvatarDesignData,
-    existingKey: string,
     pieceId: PieceId,
-    pieceVariantId: PieceVariantId,
-    rootPieceId: PieceId,
-    rootPieceVariantId: PieceVariantId
+    pieceVariantId: PieceVariantId
   ) {
     const piece = Pieces[pieceId]
     const variant = piece.variants[pieceVariantId]
-
-    const design = {
-      ...existingDesign,
-      [pieceId]: pieceVariantId,
+    const pieceVariation = {
+      pieceId: pieceId,
+      pieceVariantId: pieceVariantId,
     }
-    const key = `${existingKey}${pieceId}-${pieceVariantId}`
-    const { modifiers } = variant
 
-    if (modifiers.length === 0) {
-      const buffer = await renderPieceToPngBuffer({
-        size: OUT_SIZE,
-        design,
-        pieceId: rootPieceId,
-        variantId: rootPieceVariantId,
-        modifierPieceId: pieceId,
-        modifierVariantId: pieceVariantId,
-      })
-      await fs.writeFile(path.join(OUT_DIR, `${key}.png`), buffer)
-    } else {
-      await Promise.all(
-        modifiers.map(async (modifier) => {
-          addPiece(design, `${key}_`, modifier, rootPieceId, rootPieceVariantId)
+    const modifiersPieceVariations = variant.modifiers.map((mpid) => {
+      const piece = Pieces[mpid]
+      return Object.keys(piece.variants).map((pvid) => ({
+        pieceId: mpid,
+        pieceVariantId: pvid,
+      }))
+    })
+
+    const completeVariations = cartesianProduct(
+      ...modifiersPieceVariations
+    ).map((pvs) => [pieceVariation, ...pvs])
+
+    for (let pvs of completeVariations) {
+      const design: Partial<AvatarDesignData> = {}
+      pvs.forEach((pv) => (design[pv.pieceId] = pv.pieceVariantId))
+
+      for (let i = 0; i < pvs.length; i++) {
+        const pv = pvs[i]
+        const key = pvs
+          .map(
+            (pv, index) =>
+              `${pv.pieceId}-${pv.pieceVariantId}${index === i ? '-m' : ''}`
+          )
+          .join('_')
+        keys.push(key)
+
+        const buffer = await renderPieceToPngBuffer({
+          size: i === 0 ?  OUT_SIZE : OUT_MODIFIER_SIZE,
+          design: design as AvatarDesignData,
+          pieceId: pieceId,
+          variantId: pieceVariantId,
+          modifierPieceId: pv.pieceId,
+          modifierVariantId: pv.pieceVariantId,
         })
-      )
+        await fs.writeFile(path.join(OUT_DIR, `${key}.png`), buffer)
+      }
     }
   }
 
-  // Default values for the cases where there's multiple modifiers that affect eachother
-  const design = {
-    clothesColor: 'Blue03',
-    clothesGraphic: 'Deer',
-  } as AvatarDesignData
-
   // Add every topLevel piece
-  await Promise.all(
-    PieceIds.map(async (pieceId) => {
-      if (Pieces[pieceId].topLevel) {
-        await addPiece(design, '', pieceId)
+  for (let pieceId of PieceIds) {
+    const piece = Pieces[pieceId]
+    if (piece.topLevel) {
+      const variants = Object.keys(piece.variants)
+      for (let vid of variants) {
+        await addPieceVariant(pieceId, vid)
       }
-    })
-  )
+    }
+  }
+
+  // Write keys to file with requires for use in the app
+  let file = `
+    export default {
+      ${keys.map((key) => `"${key}": require('./${key}.png')`).join(',')}
+    }
+  `
+  await fs.writeFile(path.join(OUT_DIR, `index.ts`), file)
 
   console.log('Done')
 })()
